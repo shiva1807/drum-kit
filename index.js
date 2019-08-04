74e47a6e43597752b10d8614f74251c1e16a2c66
Shiva Kumar <kumar.shiva1807@gmail.com>
2019-08-05 00:39:25 +0530

    Finale

--- index.js
+++ index.js
@@ -51,8 +51,11 @@ function makesound(key){
 }
 
 function buttonAnimation(currentKey){
-
-}
+    var activeButton = document.querySelector("."+ currentKey)
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed")},
+        100);}
 
 
 
